87c8a8e0e663e0367b9d0e2eb87176c9708896e8
svr <svr@protonmail.com>
2021-10-20 20:10:21 +0300

    Тесты для LinkedListImpl и TwoSideLinkedList переведены на Junit

--- src/geekbrains/Main.java
+++ src/geekbrains/Main.java
@@ -1,11 +1,9 @@
 package geekbrains;
 
-import geekbrains.lesson4.test.LinkedListImplTest;
-
 public class Main {
 
     public static void main(String[] args) {
         //geekbrains.lesson3.FindMissing.test(0, 20);
-        LinkedListImplTest.run();
+        //LinkedListImplTest.run();
     }
 }
--- src/geekbrains/lesson4/test/TwoSideLinkedListTest.java
+++
@@ -1,4 +0,0 @@
-package geekbrains.lesson4.test;
-
-public class TwoSideLinkedListImplTest {
-}
